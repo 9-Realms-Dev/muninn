d54ed936c3d38a57036c2d282a8475f539a00dc1
Justin Rhoades <bfg-coding@9realmsdev.com>
2024-08-09 20:03:44 -0600

    fixed logger bug

--- internal/util/logger.go
+++ internal/util/logger.go
@@ -1,7 +1,9 @@
 package util
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 	"sync"
 
 	"github.com/charmbracelet/log"
@@ -12,9 +14,30 @@ var (
 	once   sync.Once
 )
 
+func setConfigDirectory() (string, error) {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+
+	munninDir := filepath.Join(configDir, "munnin")
+	if err := os.MkdirAll(munninDir, 0755); err != nil {
+		return "", err
+	}
+
+	munninLogFile := fmt.Sprintf("%s/debug.log", munninDir)
+
+	return munninLogFile, nil
+}
+
 func init() {
 	once.Do(func() {
-		logFile, err := os.OpenFile("debug.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		munninLogFile, err := setConfigDirectory()
+		if err != nil {
+			log.Fatal("Failed to create munnin config directory")
+		}
+
+		logFile, err := os.OpenFile(munninLogFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 		if err != nil {
 			log.Fatal("Failed to open log file:", err)
 		}
